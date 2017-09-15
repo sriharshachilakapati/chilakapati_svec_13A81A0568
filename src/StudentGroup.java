a71573d2690d6f7b30d06e38d4fc10e6f48f9d08
Sri Harsha Chilakapati <sriharshachilakapati@gmail.com>
2017-09-15 21:06:52 +0530

    Implement getStudents() and setStudents()

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -6,7 +6,7 @@ import java.util.Date;
  * after the element was removed the size of the array should be equal to the number of stored elements
  * after the element was added the size of the array should be equal to the number of stored elements
  * null elements are not allowed to be stored in the array
- * 
+ *
  * You may add new methods, fields to this class, but DO NOT RENAME any given class, interface or method
  * DO NOT PUT any classes into packages
  *
@@ -14,7 +14,7 @@ import java.util.Date;
 public class StudentGroup implements StudentArrayOperation {
 
 	private Student[] students;
-	
+
 	/**
 	 * DO NOT remove or change this constructor, it will be used during task check
 	 * @param length
@@ -25,13 +25,15 @@ public class StudentGroup implements StudentArrayOperation {
 
 	@Override
 	public Student[] getStudents() {
-		// Add your implementation here
-		return null;
+		return students;
 	}
 
 	@Override
 	public void setStudents(Student[] students) {
-		// Add your implementation here
+		if (students == null)
+            throw new IllegalArgumentException("students cannot be null");
+
+        this.students = students;
 	}
 
 	@Override
